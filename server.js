621e9dd4b138a46932c3f5b9381933f3fdc7ab7d
Michael's <mjlikre@gmail.com>
2019-07-12 14:01:02 -0700

    firebase set up

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const connection = require('./app/data/connection');
-const fs = require('fs');	
+// const fs = require('fs');	
 
 const app = express();
 
@@ -13,21 +13,21 @@ app.use(express.urlencoded({ extended: true }));
 // require("./app/routing/apiRoutes.js")(app);
 require("./app/routing/htmlRoutes.js")(app);
 
-connection.query('SELECT state, COUNT(state) FROM gun_violence GROUP BY state;', function(err, data){
-	if (err) throw err; //#endregion
-	console.log(data);
-	fs.writeFile("data.csv", JSON.stringify(data), function(err) {
+// connection.query('SELECT state, COUNT(state) FROM gun_violence GROUP BY state;', function(err, data){
+// 	if (err) throw err; //#endregion
+// 	console.log(data);
+// 	fs.writeFile("data.csv", JSON.stringify(data), function(err) {
 
-		// If the code experiences any errors it will log the error to the console.
-		if (err) {
-		  return console.log(err);
-		}
+// 		// If the code experiences any errors it will log the error to the console.
+// 		if (err) {
+// 		  return console.log(err);
+// 		}
 	  
-		// Otherwise, it will print: "movies.txt was updated!"
-		console.log("movies.txt was updated!");
+// 		// Otherwise, it will print: "movies.txt was updated!"
+// 		console.log("movies.txt was updated!");
 	  
-	  });
-});
+// 	  });
+// });
 
 // Checking if server is live
 app.listen(PORT, function () {

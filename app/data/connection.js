592bda7fab7f61db53f51a63418490aaa9591918
Michael's <mjlikre@gmail.com>
2019-07-16 01:04:04 -0700

    returns the right data

--- app/data/connection.js
+++ app/data/connection.js
@@ -1,26 +1,30 @@
-var admin = require('firebase-admin');
-var serviceAccount = require("./gun-violence-f2f2b-firebase-adminsdk-s9n3m-ae6d5e0b93.json");
+const mysql = require('mysql');
 
-console.log('firing');
+var connection 
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+    connection = mysql.createConnection({
+        host: 'localhost',
+        port: '3306',
+        user: 'root',
+        password: 'password',
+        database: 'db'
+    });
+    
+}
+connection.connect(function(err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://gun-violence-f2f2b.firebaseio.com"
-});
-// var db = admin.database();
-// var ref = db.ref();
-// ref.on("value", function (snapshot) {
-//   snapshot.forEach(function (data) {
-//     // var lat = data.val().latitude;
-//     // var long = data.val().longitude;
-//     //console.log("lat: " + lat + " long: " + long);
-//     return data;
-//   });
-//   console.log(snapshot)
-// }, function (errorObject) {
-//   console.log("The read failed: " + errorObject.code);
-// });
 
-module.exports = admin;
+
+module.exports = connection;
+
+
 
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const admin = require('../data/connection');
+const connection = require('../data/connection');
 
-var db = admin.database();
-var ref = db.ref();
+
 
 router.get('/', (req,res)=>{
-    ref.on("value", function (snapshot) {
-        snapshot.forEach(function (data) {
-          // var lat = data.val().latitude;
-          // var long = data.val().longitude;
-          //console.log("lat: " + lat + " long: " + long);
-          res.json(data);
-          return data
-        });
-        
-        
-      }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-      });
+    connection.query('SELECT * FROM gun_Data;', (err, data)=>{
+      if (err){
+        return res.status(500).end
+      }
+      res.send(data);
+    })
 })
 
 

7ec9f308b82c26c76f3ee61cd3a9a9db29a16cad
birna <briandam26@yahoo.com>
2019-08-14 21:38:26 -0700

    modified Schema, cleaned up code a bit trying to get mlab to work

--- models/incident.js
+++ models/incident.js
@@ -1,15 +1,54 @@
+"use strict";
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const IncidentSchema = new Schema({
-    incident_id: Number,
-    state: String,
-    city_or_county: String,
-    n_killed: Number,
-    n_injured: Number,
-    incident_url: String,
-    longitude: Number,
-    latitude: Number
+    incident_id: {
+        type: Number,
+        unique: true,
+        default: 0
+    },
+
+    state: {
+        type: String,
+        reqiored: true
+    },
+
+    city_or_county: {
+        type: String,
+        required: false
+    },
+
+    n_killed: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+
+    n_injured: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+
+    incident_url: {
+        type: String,
+        required: false,
+        default: null
+    },
+
+    longitude: {
+        type: Number,
+        required: false,
+        default: 0
+    },
+
+    latitude: {
+        type: Number,
+        required: false,
+        default: 0
+    }
 });
 
 const Incident = mongoose.model("Incident", IncidentSchema);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,5 @@
+"use strict";
+
 module.exports = {
     MONGODB_URI: "mongodb://birna:password123@ds263307.mlab.com:63307/heroku_whxcgd24" || "mongodb://localhost/lakeScrape",
     Incident: require("./Incident")
